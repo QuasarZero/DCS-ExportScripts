640324fcd6d54bb3fccbd05192c2ad8e5b8001bc
Quasar <lelouchcctony@gmail.com>
2025-01-18 21:34:20 +0800

    chore: F-14 refine float or to int; add launch bar abort & cover; add elev lead output

--- Scripts/DCS-ExportScript/ExportsModules/F-14B_nosaMtrevoC.lua
+++ Scripts/DCS-ExportScript/ExportsModules/F-14B_nosaMtrevoC.lua
@@ -176,15 +176,15 @@ ExportScript.ConfigEveryFrameArguments =
 
 -- A1a
 		-- Volume/TACAN Command Panel
-		[2040]  =  "%.4f",   -- ALR-67 Knob controls audio volume to pilot
-		[2039]  =  "%.4f",   -- SW knob volume of sidewinder audio tone to pilot
-		[2038]  =  "%.4f",   -- V/UHF 2 knob audio from AN/ARC-182 to pilot
+		[2040]  =  "%.1f",   -- ALR-67 Knob controls audio volume to pilot
+		[2039]  =  "%.1f",   -- SW knob volume of sidewinder audio tone to pilot
+		[2038]  =  "%.1f",   -- V/UHF 2 knob audio from AN/ARC-182 to pilot
 		[0]  =  "%.4f",   -- TACAN CMD switch sets crew member in command of Tacan
 
 		-- ICS Control Panel
-		[2048]  =  "%.4f",   -- VOL knob from RIO to pilot
-		[2045]  =  "%.4f",   -- AMP selection knob (B/U|NORM|EMER)
-		[2044]  =  "%.4f",   -- ICS switch (RADIO OVERRIDE|HOT MIC|COLD MIC)
+		[2048]  =  "%.1f",   -- VOL knob from RIO to pilot
+		[2045]  =  "%.1f",   -- AMP selection knob (B/U|NORM|EMER)
+		[2044]  =  "%1d",   -- ICS switch (RADIO OVERRIDE|HOT MIC|COLD MIC)
 
 		-- AFCS Control Panel
 		[2106]  =  "%.4f",   -- PITCH switch enables stability augmentation
@@ -196,8 +196,8 @@ ExportScript.ConfigEveryFrameArguments =
 		[2112]  =  "%.4f",   -- ENGAGE switch (ENGAGE|OFF)
 
 		-- ASYM Limiter/Engine Mode Select
-		[16006] =  "%.4f",   -- ASYM LIMITER switch (ON|OFF)
-		[16005] =  "%.4f",   -- ASYM LIMITER cover
+		[16006] =  "%1d",   -- ASYM LIMITER switch (ON|OFF)
+		[16005] =  "%1d",   -- ASYM LIMITER cover
 		[16007]  =  "%.4f",   -- L ENG MODE SELECT (PRI|SEC)
 		[16008]  =  "%.4f",   -- R ENG MODE SELECT (PRI|SEC)
 
@@ -228,12 +228,12 @@ ExportScript.ConfigEveryFrameArguments =
 		[8051]  =  "%.4f",   -- Status NO-GO
 		[2115]  =  "%.4f",   -- BIT button initiating TACAN BIT
 		[2043]  =  "%.4f",   -- NORMAL MODE switche (X|Y)
-		[2036]  =  "%.4f",   -- VOL knob for TACAN audio to pilot
+		[2036]  =  "%.1f",   -- VOL knob for TACAN audio to pilot
 		[2041]  =  "%.4f",   -- MODE konb (OFF|REC|T/R|A/A|BCN)
 		[2042]  =  "%.4f",   -- MODE (NORMAL|INVERSE)
 
 		-- UHF 1 (AN/ARC-159) Radio
-		[2031]  =  "%.4f",   -- VOL knob controls radio to pilot vol.
+		[2031]  =  "%.1f",   -- VOL knob controls radio to pilot vol.
 		[2035]  =  "%.4f",   -- SQL switch squelch (ON|OFF)
 		[2030]  =  "%.4f",   -- Freq. Select Switch 1
 		[2029]  =  "%.4f",   -- Freq. Select Switch 2
@@ -241,9 +241,9 @@ ExportScript.ConfigEveryFrameArguments =
 		[2026]  =  "%.4f",   -- Freq. Select Switch 4
 		[0]  =  "%.4f",   -- FREQ/(CHAN) display ??? (maybe the switch values above???)
 		[8115]  =  "%.4f",   -- READ button
-		[2027]  =  "%.4f",   -- BRT knob display brightness
+		[2027]  =  "%.1f",   -- BRT knob display brightness
 		[16009] =  "%.4f",   -- LOAD button
-		[2034]  =  "%.4f",   -- Function selector knob (ADF|BOTH|MAIN|OFF)
+		[2034]  =  "%.2f",   -- Function selector knob (ADF|BOTH|MAIN|OFF)
 		[2032]  =  "%.4f",   -- CHAN SEL knob selects preset channels
 		[2033]  =  "%.4f",   -- Mode selector knob (GUARD|MANUAL|PRESET)
 		[16010] =  "%.4f",   -- TONE button transmits tone on freq.
@@ -262,7 +262,7 @@ ExportScript.ConfigEveryFrameArguments =
 -- A2
 		-- Fuel Management Panel
 		[1076]  =  "%.4f",   -- QTY SEL Switch (FEED|WING|EXT)
-		[1095]  =  "%.4f",   -- Select Fuel Feed to engines (FWD|NORM|AFT)
+		[1095]  =  "%1d",   -- Select Fuel Feed to engines (FWD|NORM|AFT)
 		[1094]  =  "%.4f",   -- Fuel feed cover
 		[1001]  =  "%.4f",   -- WING/EXT Transfer (ORIDE|AUTO|OFF)
 		[15002] =  "%.4f",   -- Refueling Probe Indicator Light
@@ -357,7 +357,7 @@ ExportScript.ConfigEveryFrameArguments =
 		-- Vertical Display Indicator (VDI)
 		[1037]  =  "%.4f",   -- HUD Brightness Control
 		[1036]  =  "%.4f",   -- VDI Brightness Control
-		[1038]  =  "%.4f",   -- VDI Contrast Control
+		[1038]  =  "%.2f",   -- VDI Contrast Control
 		[1033]  =  "%.4f",   -- FILTER Handle when pulled inserts filter for night ops
 		[1034]  =  "%.4f",   -- HUD Trim control for pitch lines
 		[1035]  =  "%.4f",   -- VDI Trim adjustment of pitch lines on VDI
@@ -383,7 +383,7 @@ ExportScript.ConfigEveryFrameArguments =
 
 -- 6c
 		-- Horizontal Situation Display Indicator (HSD)
-		[1043]  =  "%.4f",   -- BRT controls HSD Brightness
+		[1043]  =  "%.2f",   -- BRT controls HSD Brightness
 		[1039]  =  "%.4f",   -- HDG controls heading reference bug in TACAN mode
 		[1040]  =  "%.4f",   -- CRS controls cour in MAN and TACAN mode
 		[1041]  =  "%.4f",   -- TEST allows reset of HSD
@@ -484,10 +484,10 @@ ExportScript.ConfigEveryFrameArguments =
 		-- END Displays Control Panel
 
 		-- gun elevation lead in mils
-		[2270]  =  "%.4f",   -- Hundreds Counter in mils for the manual A/A and A/G gun modes. Limits are -263 and +87 mils
-		[2271]  =  "%.4f",   -- Tens Counter in mils for the manual A/A and A/G gun modes. Limits are -263 and +87 mils
-		[2272]  =  "%.4f",   -- Ones Counter in mils for the manual A/A and A/G gun modes. Limits are -263 and +87 mils
-		[2273]  =  "%.4f",   -- Knob adjust counter
+		[2270]  =  "%1d",   -- Hundreds Counter in mils for the manual A/A and A/G gun modes. Limits are -263 and +87 mils
+		[2271]  =  "%1d",   -- Tens Counter in mils for the manual A/A and A/G gun modes. Limits are -263 and +87 mils
+		[2272]  =  "%1d",   -- Ones Counter in mils for the manual A/A and A/G gun modes. Limits are -263 and +87 mils
+		[2273]  =  "%.1f",   -- Knob adjust counter
 
 
 
@@ -586,7 +586,7 @@ ExportScript.ConfigEveryFrameArguments =
 		[16014] =  "%.4f",   -- HDG Button
 
 		-- ARA-63 Control Panel
-		[912]   =  "%.4f",   -- Channel Selector (1 of 20 available ICLS channels)
+		[912]   =  "%.5f",   -- Channel Selector (1 of 20 available ICLS channels) 0.05263
 		[911]   =  "%.4f",   -- BIT button
 		[910]   =  "%.4f",   -- Power Switch (ON|OFF)
 		[910]   =  "%.4f",   -- Power ON Light for ARA-63 (taken from power on/off switch)
@@ -839,6 +839,8 @@ ExportScript.ConfigArguments =
 	-- [END A3] -------------------------------------------------------------------------------
 
 	-- [A4 Pilot - Left Instrument Panel] -----------------------------------------------------
+	[496] = "%1d",   -- Launch Bar Abort Switch Cover, (1, 0, 1)
+    [497] = "%1d",   -- Launch Bar Abort, (1, 0, 1)
 	-- [END A4] -------------------------------------------------------------------------------
 	
 	-- [A5 Pilot - Left Windshield Frame] -----------------------------------------------------
@@ -875,14 +877,14 @@ ExportScript.ConfigArguments =
 		[16003] =  "%d",   	 -- MSL Mode switch (NORM|BRSIT)				- {0, 1}{0, 1}														[BUTTON ID:3141  / DEVICE ID:55 ]
 
 		-- Weapon Station Status Flags
-		[9221]  =  "%.2f",   -- Station Status Flag 1A (OFF|WHITE|CHECK)	- {0, 1}{0, 1}
-		[9222]  =  "%.2f",   -- Station Status Flag 1B (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
-		[9223]  =  "%.2f",   -- Station Status Flag 3 (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
-		[9224]  =  "%.2f",   -- Station Status Flag 4 (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
-		[9225]  =  "%.2f",   -- Station Status Flag 5 (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
-		[9226]  =  "%.2f",   -- Station Status Flag 6 (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
-		[9227]  =  "%.2f",   -- Station Status Flag 8B (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
-		[9228]  =  "%.2f",   -- Station Status Flag 8A (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
+		[9221]  =  "%.1f",   -- Station Status Flag 1A (OFF|WHITE|CHECK)	- {0, 1}{0, 1}
+		[9222]  =  "%.1f",   -- Station Status Flag 1B (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
+		[9223]  =  "%.1f",   -- Station Status Flag 3 (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
+		[9224]  =  "%.1f",   -- Station Status Flag 4 (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
+		[9225]  =  "%.1f",   -- Station Status Flag 5 (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
+		[9226]  =  "%.1f",   -- Station Status Flag 6 (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
+		[9227]  =  "%.1f",   -- Station Status Flag 8B (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
+		[9228]  =  "%.1f",   -- Station Status Flag 8A (OFF|WHITE|CHECK) 	- {0, 1}{0, 1}
 
 		-- Engine Fire Lights
 		[15014] =  "%d",	 -- Left Engine FIRE Light 						- {0, 1}{0, 1}
@@ -1324,6 +1326,17 @@ end
 function ExportScript.gunCounter(mainPanelDevice) -- Gun Counter
 	local gunCounter = ExportScript.rotaryCounter(mainPanelDevice:get_argument_value(4060)) .. ExportScript.rotaryCounter(mainPanelDevice:get_argument_value(4061)) .. ExportScript.rotaryCounter(mainPanelDevice:get_argument_value(4062))
 	ExportScript.Tools.SendData(54060, "ROUNDS\n" .. gunCounter)
+
+
+    local hLead    = ExportScript.Tools.round(mainPanelDevice:get_argument_value(2270) * 10, 0)
+    local tLead    = ExportScript.Tools.round(mainPanelDevice:get_argument_value(2271) * 10, 0)
+    local oLead    = ExportScript.Tools.round(mainPanelDevice:get_argument_value(2272) * 10, 0)
+    local sign     = ExportScript.Tools.round(mainPanelDevice:get_argument_value(2273) * 10, 0)
+    if sign > 0 then sign = "-"
+    else sign = "+"
+    end
+	local elevLead = sign .. math.abs(hLead % 10) .. math.abs(tLead  % 10) .. math.abs(oLead % 10)
+	ExportScript.Tools.SendData(54061, elevLead .. "\nMILS")
 end
 
 
